a46625ff8b6ffb32425e2d631b14f0a84c30a446
sazid <sazid19hasan98@gmail.com>
2020-12-19 21:40:13 +0600

    added logic for Showing/Hiding the Modal

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -25,7 +25,11 @@ const buildControls = (props) => (
         />
       );
     })}
-    <button className={classes.OrderButton} disabled={!props.purchasAble}>
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasAble}
+      onClick={props.ordered}
+    >
       ORDER NOW
     </button>
   </div>
