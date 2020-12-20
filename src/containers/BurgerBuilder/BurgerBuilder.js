3d3331183a2a6c2a6225edec4f334dbd96a68d91
sazid <sazid19hasan98@gmail.com>
2020-12-20 14:40:47 +0600

    adde total price in the order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -96,6 +96,7 @@ class BurgerBuilder extends Component {
         >
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice.toFixed(2)}
             purchaseCanceled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}
           />
