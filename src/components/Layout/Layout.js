4b4a05f20d80054e04fbd59ec730fbbcea437d72
sazid <sazid19hasan98@gmail.com>
2020-12-20 15:07:21 +0600

    created Navigation folder inside Components. There, created Toolbar css and js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => {
   return (
     <Aux>
+      <Toolbar />
       <div>Toolbar, Sidedrawe, Backdrop</div>
       <main className={classes.Content}>{props.children}</main>
     </Aux>
